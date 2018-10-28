83a7a733d680ca481ac6d9321bd3ede9cf2d87f9
KevinShevchuk <kevin.shevchuk@gmail.com>
2018-10-28 12:20:03 -0400

    Initial commit. Added console and a a few problems.

---
+++ ProgrammingPracticeProblems/MultiplesOf3And5.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    class MultiplesOf3And5
+    {
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        //Find the sum of all the multiples of 3 or 5 below 1000.
+
+        public MultiplesOf3And5()
+        {
+            Console.WriteLine("Input the max value to sum up to:");
+            string input = Console.ReadLine();
+            int max = 0;
+            if (int.TryParse(input, out max))
+                {
+                Console.WriteLine("----------Solution 1:-----------");
+                Solution1(max);
+                Console.WriteLine("----------Solution 2:-----------");
+                Solution2(max);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input.");
+            }
+        }
+
+        public void Solution1(int max) //O(n)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < max; i++)
+            {
+                if (i % 3 == 0) { sum += i; }
+                if (i % 5 == 0) { sum += i; }
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+
+        public void Solution2(int max)
+        {
+            int three = 0;
+            int five = 0;
+            int sum = 0;
+
+            while (three < max)
+            {
+                sum += three;
+                three += 3;
+            }
+            while (five < max)
+            {
+                sum += five;
+                five += 5;
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
--- ProgrammingPracticeProblems/Program.cs
+++ ProgrammingPracticeProblems/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,80 @@ namespace ProgrammingPracticeProblems
 {
     class Program
     {
+        private static List<Type> _classList = new List<Type>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome!");
+            GetClassList();
+            while (true)
+            {
+                Console.WriteLine("Type \"dir\" to display all the available problems. Type the name to run it.");
+                Console.WriteLine("Type \"exit\" to quit.");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "dir":
+                        PrintDir();
+                        break;
+                    case "exit":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        FindProblem(input);
+                        break;
+                        
+                }
+            }
+        }
+
+        private static void GetClassList()
+        {
+            var q = from t in Assembly.GetExecutingAssembly().GetTypes()
+                    where t.IsClass && t.Namespace == "PracticeProblems"
+                    select t;
+            _classList.AddRange(q.ToList());
+        }
+
+        private static void PrintDir()
+        {
+            Console.WriteLine("============Directory:============");
+            foreach (Type clss in _classList)
+            {
+                if (clss.Name != "Program")
+                {
+                    Console.WriteLine(clss.Name.PadLeft(4));
+                }
+            }
+            Console.WriteLine("==========End Directory:==========");
+        }
+
+        private static void FindProblem(string name)
+        {
+            var clsName = from t in _classList
+                             where t.Name == name
+                             select t;
+            if (clsName.Count() == 0)
+            {
+                Console.WriteLine("No match found or no command by that name. Please try again.");
+            }
+            else
+            {
+                Console.WriteLine("============" + clsName.First().Name + "============");
+                RunProblem(clsName.First());
+            }
+        }
+
+        private static void RunProblem(Type t)
+        {
+            try
+            {
+                Activator.CreateInstance(t);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
