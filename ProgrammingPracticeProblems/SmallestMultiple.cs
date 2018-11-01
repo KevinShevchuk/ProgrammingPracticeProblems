81c53d7c59799715b83f9050e51fd966a30b621b
KevinShevchuk <kevin.shevchuk@gmail.com>
2018-10-31 20:58:38 -0400

    New problem.

--- ProgrammingPracticeProblems/SmallestMultiple.cs
+++ ProgrammingPracticeProblems/SmallestMultiple.cs
@@ -8,6 +8,9 @@ namespace PracticeProblems
 {
     class SmallestMultiple
     {
+    //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
         public SmallestMultiple()
         {
             Console.WriteLine("Input the max value to sum up to:");
---
+++ ProgrammingPracticeProblems/SumSquareDifference.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    //The sum of the squares of the first ten natural numbers is,
+    //1^2 + 2^2 + ... + 10^2 = 385
+    //The square of the sum of the first ten natural numbers is,
+    //(1 + 2 + ... + 10)^2 = 552 = 3025
+    //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+    //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+    class SumSquareDifference
+    {
+        public SumSquareDifference()
+        {
+            Console.WriteLine("Input the number to find the sum square difference of:");
+            string input = Console.ReadLine();
+            int max = 0;
+            if (int.TryParse(input, out max))
+            {
+                Console.WriteLine("----------Solution 1:-----------");
+                Console.WriteLine(Solution1(max));
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input.");
+            }
+        }
+
+        private int Solution1(int max)
+        {
+            int sum = 0; 
+            int squ = 0;
+            for (int i = 1; i <= max; i++)
+            {
+                sum += i;
+                squ += (i * i);
+            }
+            sum *= sum;
+            return sum - squ;
+        }
+    }
+}
