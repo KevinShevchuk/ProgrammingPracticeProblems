e008acb4ef83ab23c099db85cc27c2664f938f79
kevin.shevchuk <kevin.shevchuk@gmail.com>
2019-06-12 22:32:40 -0400

    -New Problem

---
+++ ProgrammingPracticeProblems/CodeChefBeginner/IdAndShip.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticeProblems
+{
+	class IdAndShip
+	{
+        private Dictionary<char, string> _lookup = new Dictionary<char, string>()
+        {
+            { 'B', "Battleship"},
+            { 'C', "Cruiser" },
+            { 'D', "Destroyer" },
+            { 'F', "Frigate" }
+        };
+
+        public IdAndShip()
+        {
+            Console.WriteLine("Input the number of tests:");
+            string input = Console.ReadLine();
+            bool success = int.TryParse(input, out int numberOfTests);
+            if (success && numberOfTests >= 1 && numberOfTests <= 100)
+            {
+                string[] results = new string[numberOfTests];
+                int inputs = 0;
+                while (inputs < numberOfTests)
+                {
+                    Console.WriteLine("Enter the Id:");
+                    string testInput = Console.ReadLine().ToUpper();
+                    char test = testInput[0];
+                    if (test >= 'A' && test <= 'Z')
+                    {
+
+                        results[inputs] = Solution1(test);
+                        inputs++;
+                    }
+                }
+                foreach (string i in results)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input.");
+            }
+        }
+
+        private string Solution1(char input)
+        {
+            return _lookup[input];
+        }
+    }
+}
